46aa64dfe770e51aee3f06bfe3f7bd062906b7fb
mustafa <msolmaz83@gmail.com>
2023-09-29 23:40:45 +0200

    zweiperson

--- sayitahmin/index.js
+++ sayitahmin/index.js
@@ -2,20 +2,21 @@
 const pcB = Math.ceil(Math.random()*20)
 console.log(pcB)
 
-
-
-
 // guess.addEventListener('keyup',()=>{
 //     let df = ''
 //      df = guess.value
 //      console.log(guess.value)
 //      console.log(df,'df')
 // })
+
+
+
 var sayi = 10;
 const check = document.querySelector('.check')
 check.addEventListener('click',()=>{
     sayi--;
-    console.log(sayi)
+    ikinci(sayi)
+    
 let guess = document.querySelector('.guess').value
 if(sayi >= 0){ 
     if(guess == '') alert('sayi giriniz')
@@ -37,5 +38,24 @@ if(sayi >= 0){
 }
    else document.querySelector('.msg').textContent ='kaybettiniz'
     
-
 })
+
+
+const ikinci = (sayi)=>{
+    console.log(document.querySelector('.score').textContent)
+    if(sayi == 0){
+        document.querySelector('.again').textContent = 'zwei'
+        document.querySelector('.msg').textContent ='yeni yarismayica basarilar'
+    }
+}
+
+const ty = document.querySelector('.again')
+
+if(document.querySelector('.again').textContent = 'zwei'){
+    console.log('first')
+    ty.addEventListener('click',()=>{
+        console.log('tikladik sukur')
+    })
+
+}
+
