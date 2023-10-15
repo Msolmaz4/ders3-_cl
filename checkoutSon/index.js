19e3e6f168dcfca82aa0270c1b50aef8946614da
mustafa <msolmaz83@gmail.com>
2023-10-15 23:42:31 +0200

    total

--- checkoutSon/index.js
+++ checkoutSon/index.js
@@ -33,5 +33,23 @@ const removeCartItem = (event)=>{
     console.log(event)
     const buttonClicked = event.target
     buttonClicked.parentElement.remove()
+    total()
 
 }
+
+const total = ()=>{
+    const cartContent = document.getElementsByClassName('cart-content')[0]
+    const cartBoxes = cartContent.getElementsByClassName('cart-box')
+    console.log(cartBoxes)
+    let total = 0
+    for(let i = 0;i< cartBoxes.length ;i++){
+        let cartBox = cartBoxes[i]
+        let priceElement = cartBox.getElementsByClassName('cart-price')[0]
+        let quantityElement = cartBox.getElementsByClassName('cart-quantity')[0]
+        let price = parseFloat(priceElement.innerText.replace('$',''))
+        let quantity = quantityElement.value 
+        total = total+(price*quantity)
+        document.getElementsByClassName('total-price')[0].innerText = '$'+total
+    }
+}
+
