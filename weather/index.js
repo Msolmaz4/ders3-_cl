03ae1e84953f52ba5e3c65ddf6c85aa4a3e0f10c
mustafa <msolmaz83@gmail.com>
2023-10-19 11:21:42 +0200

    delete

--- weather/index.js
+++ weather/index.js
@@ -58,55 +58,65 @@
 
 let box = []
 
+
 const inp = document.querySelector('.input')
 const btn = document.querySelector('.submit')
 const card = document.querySelector('.card')
 const clos = document.querySelectorAll('.card_close')
 btn.addEventListener('click',async()=>{
-    const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inp.value}&appid=27003ec5cbbd70ae707c6360ae1d1bed`)
+    
+    const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inp.value}&appid=27003ec5cbbd70ae707c6360ae1d1bed&units=metric`)
     const veri = await data.json()
-    box.push(veri)
-    ekrana()
-    inp.value = ''
-
+    if(box.length < 4 ){
+      box.push(veri)
+      ekrana()
+    }
+    else{
+        alert('konnen sie ein Cart losen')
+    } 
+    console.log(box)
+    inp.value= ''
 })
 
-const screnn = ()=>{
-    
+const ekrana = ()=>{
+    card.innerHTML = ''
+    const dert =  box.filter((item)=>item.name.toLowerCase() !=inp.value.toLowerCase())
+    //console.log(inp.value.toLowerCase())
+    console.log(dert)
+
     box.map((er)=>{
-        card.innerHTML += `
+      card.innerHTML += `
          <div class='card_in'>
            <div class='card_close'>x</div>
          <div class='card_mitte'>
             <h2>${er.name}</h2>
-            <span>${er.main.feels_like}</span>
-            <span>${er.weather[0].description}</span>
+            <span class='feel'>${er.main.feels_like}&deg;C</span>
+            <span class='desc'>${er.weather[0].description}</span>
          </div>
+         <div class='res'>
+            <img src= ${`https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${er.weather[0].icon}.svg`} />
+        </div>
          <div class='son'>
              <span>${er.sys.country}</span>
         </div>
         </div>
         `
     })
-
-}
-
-const ekrana = ()=>{
-    card.innerHTML = ''
-
-    if(box.length < 5){
-        screnn()
-    }else{
-        alert('konnen sie ein Cart losen')
-        screen()
-    }
+ 
 }
 
 card.addEventListener('click', (event) => {
     if (event.target.classList.contains('card_close')) {
-        console.log(event.target.parentElement);
+        console.log(event.target.parentElement.querySelector('h2').innerHTML);
+        box = box.filter((item)=>item.name !=event.target.parentElement.querySelector('h2').innerHTML )
+        console.log(box)
         event.target.parentElement.remove()
-
+        ekrana()
     }
 });
 
+inp.addEventListener('keyup',(e)=>{
+    if(e.keyCode === 13){
+        btn.click()
+    }
+})
